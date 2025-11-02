37a529c0f80a0d73b0e8cf23f58688d951a6e255
Jackson Hall <jacksonthall22@gmail.com>
2025-11-02 07:49:29 -0500

    Tweak output sorting

--- src/san_strings/__init__.py
+++ src/san_strings/__init__.py
@@ -196,8 +196,17 @@ def gen_sans() -> set[str]:
 def main():
     all_sans = gen_sans()
 
-    def sort_key(s):
-        return len(s), s
+    def sort_key(s: str) -> tuple:
+        def is_check(s):
+            return s.endswith('+')
+
+        def is_mate(s):
+            return s.endswith('#')
+
+        def is_plain(s):
+            return not is_check(s) and not is_mate(s)
+
+        return is_mate(s), is_check(s), is_plain(s), len(s), s
 
     all_sans = sorted(all_sans, key=sort_key)
 
