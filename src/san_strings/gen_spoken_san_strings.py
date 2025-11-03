74b1194a42045e86aa9be10b277c25c49cd32c2d
Jackson Hall <jacksonthall22@gmail.com>
2025-11-03 03:38:49 -0500

    Update `spoken_san_strings.txt`

--- src/san_strings/gen_spoken_san_strings.py
+++ src/san_strings/gen_spoken_san_strings.py
@@ -7,58 +7,66 @@ with open('san_strings.txt', 'r') as f:
 spoken_san_strings = set()
 
 for i, san_string in enumerate(san_strings):
-    words = []
+    words: list[str | tuple[str, ...]] = []
 
-    idx = 0
-    while idx < len(san_string):
-        c = san_string[idx]
+    if san_string.startswith('O-O-O'):
+        words.append(('castles', 'castles queenside'))
+    elif san_string.startswith('O-O'):
+        words.append(('castles', 'castles kingside'))
+    else:
+        idx = 0
+        while idx < len(san_string):
+            c = san_string[idx]
 
-        if c.isupper():
-            if c == 'N':
-                words.append('knight')
-            elif c == 'B':
-                words.append('bishop')
-            elif c == 'R':
-                words.append('rook')
-            elif c == 'Q':
-                words.append('queen')
-            elif c == 'K':
-                words.append('king')
-        elif c == 'x':
-            if san_string[0].islower():
-                # Pawn captures only
-                # Ex. 'e takes d 5' or 'e d 5'
-                assert len(words) == 1
-                file = words.pop()
-                words.append((f'{file} takes', 'takes'))
-            else:
-                # Non-pawn caputures
-                if len(words) == 1:
-                    piece = words.pop()
-                    words.append((f'{piece} takes', 'takes'))
+            if c.isupper():
+                if c == 'N':
+                    words.append('knight')
+                elif c == 'B':
+                    words.append('bishop')
+                elif c == 'R':
+                    words.append('rook')
+                elif c == 'Q':
+                    words.append('queen')
+                elif c == 'K':
+                    words.append('king')
                 else:
-                    words.append('takes')
+                    raise RuntimeError(f'Invalid character {c}')
+            elif c == 'x':
+                if san_string[0].islower():
+                    # Pawn captures only
+                    # Ex. 'e takes d 5' or 'e d 5'
+                    assert len(words) == 1
+                    file = words.pop()
+                    words.append((f'{file} takes', 'takes'))
+                else:
+                    # Non-pawn captures
+                    if len(words) == 1:
+                        piece = words.pop()
+                        words.append((f'{piece} takes', 'takes'))
+                    else:
+                        words.append('takes')
 
-            # Destination of capture
-            file, rank = san_string[idx + 1 : idx + 3]
-            assert file in 'abcdefgh'
-            assert rank in '12345678'
-            idx += 3
+                # Destination of capture
+                file = san_string[idx + 1]
+                rank = san_string[idx + 2]
+                assert file in 'abcdefgh'
+                assert rank in '12345678'
+                idx += 3
 
-            # Ex. 'rook takes' or 'rook takes e 4' or 'rook takes rook'
-            words.append(
-                ('', f'{file} {rank}', 'pawn', 'knight', 'bishop', 'rook', 'queen')
-            )
-        elif c == '=':
-            words.append(('', 'equals'))
-            # break
-        else:
-            words.append(c)
+                # Ex. 'rook takes' or 'rook takes e 4' or 'rook takes rook'
+                words.append(
+                    ('', f'{file} {rank}', 'pawn', 'knight', 'bishop', 'rook', 'queen')
+                )
+            elif c == '=':
+                words.append(('', 'equals'))
+                # break
+            else:
+                words.append(c)
 
-        idx += 1
+            idx += 1
 
     if all(isinstance(w, str) for w in words):
-        spoken_san_strings.add(' '.join(words))
+        spoken_san_strings.add(' '.join(words))  # type: ignore
         continue
 
     # Enumerate every combination of different ways of saying each phrase
